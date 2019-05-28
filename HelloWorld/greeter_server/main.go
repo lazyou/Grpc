b4c96f443cc73a1feee5b3f328ecadf17647fe32
lin <test@test.com>
2019-05-28 15:43:10 +0800

    remove useles code

--- HelloWorld/greeter_server/main.go
+++ HelloWorld/greeter_server/main.go
@@ -1,21 +1,3 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 //go:generate protoc -I ../helloworld --go_out=plugins=grpc:../helloworld ../helloworld/helloworld.proto
 
 // Package main implements a server for Greeter service.
@@ -35,13 +17,14 @@ const (
 )
 
 // server is used to implement helloworld.GreeterServer.
-type server struct{}
+type server struct {
+}
 
 // SayHello implements helloworld.GreeterServer
 // 实现 GreeterServer 服务: 接收 HelloRequest, 返回 HelloReply.
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.Name)
-	return &pb.HelloReply  {
+	return &pb.HelloReply{
 		Message: "Hello " + in.Name,
 	}, nil
 }
