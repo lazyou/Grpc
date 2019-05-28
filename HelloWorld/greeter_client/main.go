54a4024f6e8f8566edea4e922f7d37165b22fe28
lin <test@test.com>
2019-05-28 16:18:28 +0800

    client\main: 连接 RPC 服务, 调用服务, 打印服务返回值

--- HelloWorld/greeter_client/main.go
+++ HelloWorld/greeter_client/main.go
@@ -24,6 +24,8 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
+
+	// 与 RPC 服务器连接的客户端
 	c := pb.NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
@@ -31,12 +33,16 @@ func main() {
 	if len(os.Args) > 1 {
 		name = os.Args[1]
 	}
+
+	// TODO: context 这部分看不懂
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
+	// 客户端调用 RPC 方法, 接收服务端返回值
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
+
 	log.Printf("Greeting: %s", r.Message)
 }
